95d6544bce11153f514f3df7c683ed82afa9c93d
Hecedu <hector.maganahdez@gmail.com>
2020-11-04 12:48:46 -0700

    Implemented joinGame callback url

--- src/Risk.SampleClient/Client.cs
+++ src/Risk.SampleClient/Client.cs
@@ -8,6 +8,8 @@ using Risk.Api.Controllers;
 using Risk.Shared;
 using System.Net.Http;
 using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using System.Web;
 
 namespace Risk.SampleClient
 {
@@ -22,11 +24,14 @@ namespace Risk.SampleClient
         string playerName { get; set; }
         Uri callback { get; set; }
 
-        
+
+        public static HttpRequest Request { get; }
+
+        string baseUrl = string.Format("{0}://{1}{2}", Request.Scheme, Request.Host, Request.PathBase);
         public async Task OnGet (string playerName )
         {
             var client = _httpClientFactory.CreateClient("RiskApi");
-            var parameters = new Dictionary<string, string> { { "playerName", playerName },{ "callback", callback.ToString()}};
+            var parameters = new Dictionary<string, string> { { "playerName", playerName },{ "callback", baseUrl.ToString()}};
             var encodedContent = new FormUrlEncodedContent(parameters);
             await client.PostAsync("server/join", encodedContent);
         }
