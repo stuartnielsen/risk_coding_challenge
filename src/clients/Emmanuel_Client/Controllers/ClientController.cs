b339c78bfbae669b7ebd551b4b0eb22e8ab31fc7
Ofnir01 <emmanuel.torresamaya@gmail.com>
2020-11-11 23:04:32 -0700

    Created the Emmanuel_Client.

---
+++ src/clients/Emmanuel_Client/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emmanuel_Client.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly IHttpClientFactory clientFactory;
+
+        public ClientController(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
+
+        [HttpGet("AreYouThere")]
+        public string AreYouThere()
+        {
+            return "Yes";
+        }
+    }
+}
