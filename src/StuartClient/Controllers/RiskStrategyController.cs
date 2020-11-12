056941dab902312d4bbe32b3ea1a28ad3af1e8fd
Stuart <nielsen.stuart9@gmail.com>
2020-11-12 10:50:02 -0700

    Client created, game logic started

---
+++ src/StuartClient/Controllers/RiskStrategyController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Risk.Shared;
+using System.Net.Http.Json;
+
+namespace StuartClient.Controllers
+{
+    public class RiskStrategyController : Controller
+    {
+        private readonly IHttpClientFactory clientFactory;
+        private static string serverAddress;
+        private GameStrategy gameStrategy;
+
+        public RiskStrategyController(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
+
+        [HttpGet("AreYouThere")]
+        public string AreYouThere()
+        {
+            return "yes";
+        }
+
+        [HttpGet("/joinServer/{*server}")]
+        public async Task<IActionResult> JoinAsync(string server)
+        {
+            serverAddress = server;
+            var client = clientFactory.CreateClient();
+            string baseUrl = string.Format("{0}://{1}{2}", Request.Scheme, Request.Host, Request.PathBase);
+            var joinRequest = new JoinRequest {
+
+                CallbackBaseAddress = baseUrl,
+                Name = "Stuart"
+            };
+            try
+            {
+                var joinResponse = await client.PostAsJsonAsync($"{serverAddress}/join", joinRequest);
+                var content = await joinResponse.Content.ReadAsStringAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+
+        [HttpPost("joinServer")]
+        public async Task<IActionResult> JoinAsync_Post(string server)
+        {
+            await JoinAsync(server);
+            return RedirectToPage("/GameStatus", new { servername = server });
+        }
+
+
+
+        [HttpPost("deployArmy")]
+        public DeployArmyResponse DeployArmy([FromBody] DeployArmyRequest deployArmyRequest)
+        {
+            //return gameStrategy.DecideArmyWhereToPlacement(deployArmyRequest);
+            DeployArmyResponse response = new DeployArmyResponse();
+            response.DesiredLocation = new Location(1, 1);
+            return response;
+        }
+
+        [HttpPost("beginAttack")]
+        public BeginAttackResponse BeginAttack([FromBody] BeginAttackRequest beginAttackRequest)
+        {
+            BeginAttackResponse response = new BeginAttackResponse();
+            response.From = new Location(1, 1);
+            response.To = new Location(1, 2);
+            return response;
+        }
+
+        [HttpPost("continueAttack")]
+        public ContinueAttackResponse ContinueAttack([FromBody] ContinueAttackRequest continueAttackRequest)
+        {
+            ContinueAttackResponse response = new ContinueAttackResponse();
+            response.ContinueAttacking = true;
+
+            return response;
+        }
+
+        [HttpPost("gameOver")]
+        public IActionResult GameOver([FromBody] GameOverRequest gameOverRequest)
+        {
+            return Ok(gameOverRequest);
+        }
+
+
+    }
+
+
+}
---
+++ src/StuartClient/GameStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Risk.Shared;
+
+namespace StuartClient
+{
+    public class GameStrategy
+    {
+        public DeployArmyResponse DecideArmyWhereToPlacement(DeployArmyRequest deployRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BeginAttackResponse DecideWhereToAttack(BeginAttackRequest attackRequest)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public ContinueAttackResponse DecideToContinueAttack(ContinueAttackRequest continueAttack)
+        { 
+            throw new NotImplementedException();
+
+        }
+    }
+}
