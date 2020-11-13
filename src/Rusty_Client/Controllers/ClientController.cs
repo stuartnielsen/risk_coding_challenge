7d671f67b807650c06b5e191987ee51bb1b70700
Rusty Hermansen <dr.delight@gmail.com>
2020-11-13 12:44:47 -0700

    Added logic for placing armies and determining when to attack. Commiting before mergin main into this branch.

--- src/Rusty_Client/Controllers/ClientController.cs
+++ src/Rusty_Client/Controllers/ClientController.cs
@@ -64,18 +64,65 @@ namespace Rusty_Client.Controllers
         [HttpPost("deployArmy")]
         public DeployArmyResponse DeployArmy([FromBody] DeployArmyRequest deployArmyRequest)
         {
-            DeployArmyResponse response = new DeployArmyResponse();
-            response.DesiredLocation = new Location(1, 1);
-            return response;
+            return createDeployResponse(deployArmyRequest);
         }
 
+        private DeployArmyResponse createDeployResponse(DeployArmyRequest deployArmyRequest)
+        {
+            DeployArmyResponse response = new DeployArmyResponse();
+            foreach(Territory space in deployArmyRequest.Board)
+            {
+                if(space.Location.Row == space.Location.Column && (space.Owner.Name == null || space.Owner.Name=="Rusty"))
+                {
+                    if(space.Owner.Name=="Rusty" && space.Armies <= 3)
+                    {
+                        response.DesiredLocation = space.Location;
+                        continue;
+                    }
+                    else
+                    {
+                        response.DesiredLocation = space.Location;
+                    }
+                    
+                }
+                return response;
+            }
+            return null;
+
+        }
         [HttpPost("beginAttack")]
         public BeginAttackResponse BeginAttack([FromBody] BeginAttackRequest beginAttackRequest)
         {
+            return createAttackResponse(beginAttackRequest); ;
+        }
+        private BeginAttackResponse createAttackResponse(BeginAttackRequest beginAttackRequest)
+        {
             BeginAttackResponse response = new BeginAttackResponse();
-            response.From = new Location(1, 1);
-            response.To = new Location(1, 2);
-            return response;
+            var attackerLocation = new Location();
+            //from is the attacker to is the defender
+            foreach(Territory space in beginAttackRequest.Board)
+            {
+                if (space.Owner.Name == "Rusty")
+                {
+                    attackerLocation = space.Location;
+                    //look at the next location to the right, left, up, down, up-right diagonal, 
+                    //down-right diagonal, up-left diagonal, down-left diagonal
+                    for(int i=space.Location.Column-1; i <= (space.Location.Column + 1); i++)
+                    {
+                        for(int j=space.Location.Row-1;j<=(space.Location.Row+1); j++)
+                        {
+                            if (space.Owner.Name != "Rusty")
+                            {
+                                response.From = attackerLocation;
+                                response.To = space.Location;
+                                return response;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            return null;
         }
 
         [HttpPost("continueAttack")]
@@ -94,23 +141,23 @@ namespace Rusty_Client.Controllers
         }
      
 
-        private BeginAttackResponse createAttackResponse(BeginAttackRequest beginAttack)
-        {
-            var from = new Location();
-            var to = new Location();
-                foreach(var territory in beginAttack.Board)
-            {
-                if(!(territory.Owner.Name is null)&& territory.Owner.Name == "Rusty")
-                {
-                    from = territory.Location;
-                }
-                if(!(from is null && to is null))
-                {
-                    break;
-                }
-            }
-            return new BeginAttackResponse { From = from, To = to };
-        }
+        //private BeginAttackResponse createAttackResponse(BeginAttackRequest beginAttack)
+        //{
+        //    var from = new Location();
+        //    var to = new Location();
+        //        foreach(var territory in beginAttack.Board)
+        //    {
+        //        if(!(territory.Owner.Name is null)&& territory.Owner.Name == "Rusty")
+        //        {
+        //            from = territory.Location;
+        //        }
+        //        if(!(from is null && to is null))
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    return new BeginAttackResponse { From = from, To = to };
+        //}
         private DeployArmyResponse createDeployRequest(DeployArmyRequest deployArmyRequest)
         {
             var location = new Location();
