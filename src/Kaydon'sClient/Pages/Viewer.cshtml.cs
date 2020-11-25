aabfdcc7b553b73ad187457d851452804cec20a0
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2020-11-25 12:01:36 -0700

    created the grid for the viewer

---
+++ src/Kaydon'sClient/Pages/Viewer.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kaydon_sClient.Pages
+{
+    public class ViewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
