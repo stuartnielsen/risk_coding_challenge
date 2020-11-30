279856cd4ec5117acc94edc8684b5500db80338d
Kaydon stubbs <k.rydge.stubbs@gmail.com>
2020-11-30 10:43:06 -0700

    I was having an error on startup with the origianla client so I created a differnet project.

---
+++ KaydonVisualizer/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Risk.Shared;
+
+namespace KaydonVisualizer.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+        private IHttpClientFactory httpClientFactory;
+        private IConfiguration configuration;
+        public GameStatus GameStatus { get; set; }
+        public int MaxRow { get; set; }
+        public int MaxCol { get; set; }
+
+        public IndexModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.configuration = configuration;
+        }
+
+        public async Task OnGetAsync()
+        {
+            GameStatus = await httpClientFactory
+                .CreateClient()
+                .GetFromJsonAsync<GameStatus>($"{configuration["GameServer"]}/status");
+            MaxRow = GameStatus.Board.Max(t => t.Location.Row);
+            MaxCol = GameStatus.Board.Max(t => t.Location.Column);
+        }
+    }
+}
